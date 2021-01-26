9adbbfd3ef16acc70523f1fd3de715c289abc731
michellcdmd@gmail.com <michellcdmd@gmail.com>
2021-01-26 12:18:57 -0300

    adição de imagem x-man no head

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -6,7 +6,8 @@ function IndexPage() {
       <Head>
         <title>Quiz Marvel</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta property="og:url" content="https://marvelquiz.repositorio2014.vercel.app/"/>        
+        <meta property="og:url" content="https://marvelquiz.repositorio2014.vercel.app/x-man.jpg"/>   
+        <meta property="og:image:type" content="../src/img/x-man.jpg"></meta>     
       </Head>
       <p>Quiz Marvel</p>
     </div>
